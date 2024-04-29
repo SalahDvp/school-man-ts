259e7173c93fa44baa3db8f12df85045b4f58891
youcef <youceftebbi@outlook.com>
2024-04-29 21:45:36 +0300

    new

--- app/(home)/dashboard/page.tsx
+++ app/(home)/dashboard/page.tsx
@@ -20,7 +20,7 @@ import { RecentSales } from "@/app/(home)/dashboard/components/recent-sales"
 
  function Page() {
   return (
-  <div className="flex-1 space-y-4 p-8 pt-6">
+  <div className="flex-1 space-y-4 p-8 pt-6" >
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
             <div className="hidden flex items-center space-x-2">
